ce2ab1c409a52aa926ba63bbe74ff7a252612036
Siôn Le Roux <sinisterstuf@gmail.com>
2014-09-28 23:29:09 +0200

    Add a simple test
    Using mocha, test that a client receives the welcome message from the
    server on connecting.

---
+++ test.js
@@ -0,0 +1,16 @@
+var server = require('./server.js')
+var net = require('net')
+require('should');
+
+describe("server", function() {
+  it("display welcome banner on join", function(done) {
+
+    var alice = net.connect({ port: 1337 })
+    alice.on('data', function(data) {
+      data.toString().should.containEql('Epic Chat Server')
+      alice.end()
+      done()
+    })
+
+  })
+})
