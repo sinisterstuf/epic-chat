d4f86a07efd3ef59bf9eb99f0067b4eef4e3f9c3
Siôn Le Roux <sinisterstuf@gmail.com>
2014-10-06 23:59:15 +0200

    Test \nick command

--- test.js
+++ test.js
@@ -4,14 +4,27 @@ var net = require('net')
 require('should')
 
 describe('server', function() {
-  it('display welcome banner on join', function(done) {
-
+  it('displays welcome banner on join', function(done) {
+    require('./server')
     var alice = net.connect({ port: config.port })
+
     alice.on('data', function(data) {
       data.toString().should.containEql('Epic Chat Server')
       alice.end()
       done()
     })
+  })
 
+  it('shows 127.0.0.1 as default nick', function(done) {
+    require('./server')
+    var alice = net.connect({ port: config.port })
+
+    alice.on('data', function(data) {
+      if (data.toString().indexOf('your name is: 127.0.0.1') > -1) {
+        done()
+      }
+    })
+
+    alice.write('\\nick\n')
   })
 })
