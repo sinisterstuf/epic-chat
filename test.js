c37a4544d835a55bdfa7a6bba1c815e3816501ad
Siôn Le Roux <sinisterstuf@gmail.com>
2014-10-15 23:48:56 +0200

    Add tests for \nick command and basic chat
    Now the following things also have working tests:
        * basic chat (sending/receiving a 'hi' message)
        * \nick command gets/sets nickname
    
    The tests under 'commands' now use beforeEach and afterEach helpers!

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ var net = require('net')
 require('should')
 
 describe('server', function() {
+
   it('displays welcome banner on join', function(done) {
     require('./server')
     var alice = net.connect({ port: config.port })
@@ -25,5 +26,70 @@ describe('server', function() {
     })
 
     alice.write('\\nick\n')
+  })
+
+  describe('commands', function() {
+    var alice
+
+    beforeEach(function() {
+      require('./server')
+      alice = net.connect({ port: config.port })
+    })
+
+    afterEach(function() {
+      alice.end()
+    })
+
+    it('shows 127.0.0.1 as default nick', function(done) {
+      alice.once('data', function(data) {
+        data.toString().should.containEql('Epic Chat Server')
+
+        alice.once('data', function(data) {
+          data.toString().should.containEql('your name is: 127.0.0.1')
+          done()
+        })
+      })
+
+      alice.write('\\nick\n')
+    })
+
+    it('changes nick with \\nick', function(done) {
+      alice.once('data', function(data) {
+        data.toString().should.containEql('Epic Chat Server')
+
+        alice.write('\\nick alice\n', 'utf8', function nickcall() {
+          alice.write('\\nick\n')
+        })
+
+        alice.once('data', function(data) {
+          data.toString().should.containEql('alice')
+          done()
+        })
+
+      })
+    })
+
+
+  })
+
+
+  it('prints messages to all but the sender', function(done) {
+    require('./server')
+    var bob = net.connect({ port: config.port })
+    var alice = net.connect({ port: config.port })
+
+    alice.once('data', function(data) {
+      data.toString().should.containEql('Epic Chat Server')
+
+      alice.once('data', function(data) {
+        data.toString().should.containEql('hi')
+        alice.end()
+        bob.end()
+        done()
+      })
+
+      bob.write('hi')
+    })
+
   })
 })
